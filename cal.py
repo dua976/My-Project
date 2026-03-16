c3901cc966fea5c2a9066b475232cc0344960d9b
dua976 <girlmuslim262@gmail.com>
2026-03-16 09:56:32 -0700

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,13 @@
+num1=float(input("Enter 1st num:"))
+num2=float(input("Enter 2nd num:"))
+operation=input("Select operation(+,-,*,/):")
+if operation == "+":
+ print("Result:",num1+num2)
+elif operation == "-":
+ print("Result:", num1-num2)
+elif operation == "*":
+ print("Result:", num1*num2)
+elif operation == "/":
+ print("Result:", num1/num2)
+else:
+ print("Invalid operation")4
